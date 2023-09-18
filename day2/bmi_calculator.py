5b6edbaf853a9e9f40304d4f867212c38aed0f38
WellsJhones <wellsjhones@gmail.com>
2023-09-18 03:44:48 -0300

    little correction on file bmi

--- day2/bmi_calculator.py
+++ day2/bmi_calculator.py
@@ -6,12 +6,12 @@ result = round(bmi)
 # updated version 9/17/23
 
 if result < 18.5:
-    print(f"your BMI is {result}: you're underweight")
+    print(f"your BMI is {result}: you're underweight.")
 elif result < 25:
-    print(f"your BMI is {result}: you have a normal weight")
+    print(f"your BMI is {result}: you have a normal weight.")
 elif result < 30:
-    print(f"your BMI is {result}: you're overweight")
+    print(f"your BMI is {result}: you're overweight.")
 elif result < 35:
-    print(f"your BMI is {result}: you're obese")
+    print(f"your BMI is {result}: you're obese.")
 else:
     print(f"your BMI is {result}:you're clinical obese.")
