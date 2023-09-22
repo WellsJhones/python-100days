a2c53b6025c24e95df802a44bb1b01cd42aa10d7
WellsJhones <wellsjhones@gmail.com>
2023-09-22 19:51:51 -0300

    remove comments

--- day5/highest_score.py
+++ day5/highest_score.py
@@ -3,9 +3,7 @@ student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
-# 🚨 Don't change the code above 👆
 
-# Write your code below this row 👇
 highest_score = 0
 for max in student_scores:
     if max > highest_score:
