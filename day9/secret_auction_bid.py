f563e72a687f33adcc33feb741e31089ab76c3a0
WellsJhones <wellsjhones@gmail.com>
2023-09-26 04:19:27 -0300

    implement: check name check valid bid.

--- day9/secret_auction_bid.py
+++ day9/secret_auction_bid.py
@@ -20,21 +20,28 @@ winner = {
 }
 while ask == True:
     name = input("what's your name? \n")
-    bid = int(input("What's the bid price: \n"))
+    if name == "" or len(name) < 2:
 
-    add_bids(name, bid)
+        print("you have to put your name and it should be identifiable with at least 2 letters!")
+    else:
+        bid = int(input("What's the bid price: \n"))
 
-    add_more_people = input(
-        "more people will participate or update your bid? 'Yes' or 'no' \n").lower()
+        if bid < 1:
+            print("Invalid number, let's start again!")
+        else:
+            add_bids(name, bid)
 
-    if add_more_people == "no":
-        ask = False
-        for n in range(len(bid_data)):
-            for key in bid_data[n]:
-                # print(bid_data[n]['bid'] > winner['bid'])
-                if bid_data[n]['bid'] > winner["bid"]:
-                    winner['name'] = bid_data[n]["name"]
-                    winner['bid'] = bid_data[n]['bid']
-                #     print()
-    print("The winner is {} with the highest bid of {}".format(
-        winner["name"], winner["bid"]))
+            add_more_people = input(
+                "more people will participate or update your bid? 'Yes' or 'no' \n").lower()
+
+            if add_more_people == "no":
+                ask = False
+                for n in range(len(bid_data)):
+                    for key in bid_data[n]:
+                        # print(bid_data[n]['bid'] > winner['bid'])
+                        if bid_data[n]['bid'] > winner["bid"]:
+                            winner['name'] = bid_data[n]["name"]
+                            winner['bid'] = bid_data[n]['bid']
+                        #     print()
+            print("The winner is {} with the highest bid of {}".format(
+                winner["name"], winner["bid"]))
