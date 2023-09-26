a40d578dc0833c51dfc48173e01ae95425cfd3b0
WellsJhones <wellsjhones@gmail.com>
2023-09-26 06:28:03 -0300

    optimized the code

--- day10/calculator.py
+++ day10/calculator.py
@@ -57,35 +57,50 @@ operations = {
 
 num1 = int(input("What's the first number?: "))
 
+simbols = []
+
 
 def show_operation():
     for operation in operations:
+        simbols.append(operation)
         print(operation)
 
 
 show_operation()
 operation_simb = input("pick an operation from the line above: ")
-num2 = int(input("what's the second number?: "))
+if operation_simb not in simbols:
+    print("invalid")
+else:
+    num2 = int(input("what's the second number?: "))
 
+    def get_result(nu1, nu2):
+        for simbol in operations:
+            if operation_simb == simbol:
+                answe = operations[simbol](nu1, nu2)
+                return answe
 
-def get_result(nu1, nu2):
-    for simbol in operations:
-        if operation_simb == simbol:
-            answe = operations[simbol](nu1, nu2)
-            return answe
-
-
-first_result = get_result(num1, num2)
-result_final = first_result
-add_more = True
-while add_more:
-    ask = input("Do you want to do more calculations? 'yes' or 'no': ").lower()
-    if ask == "yes":
-        show_operation()
-        operation_simb = input("pick an operation from the line above: ")
-        num3 = int(input("type the number: "))
-        new_result = (get_result(first_result, num3))
-        result_final = new_result
-    elif ask == 'no':
-        add_more = False
-        print(f"the result is: {result_final}")
+    first_result = get_result(num1, num2)
+    result_final = first_result
+    add_more = True
+    while add_more:
+        ask = input(
+            "Do you want to do more calculations? 'yes' or 'no': ").lower()
+        possibilities = ('yes', 'no')
+        if ask not in possibilities:
+            print("invalid choice")
+        else:
+            if ask == "yes":
+                show_operation()
+                operation_simb = input(
+                    "pick an operation from the line above: ")
+                if operation_simb not in simbols:
+                    add_more = False
+                    print("We'll stop right here, the result until now is: {}".format(
+                        result_final))
+                else:
+                    num3 = int(input("type the number: "))
+                    new_result = (get_result(first_result, num3))
+                    result_final = new_result
+            elif ask == 'no':
+                add_more = False
+                print(f"the result is: {result_final}")
