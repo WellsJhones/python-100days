e8ead92d13de8d44e40d842b3f89c835ff68ea2c
WellsJhones <wellsjhones@gmail.com>
2023-12-13 19:56:21 -0300

    turtle

---
+++ day19/main.py
@@ -0,0 +1,30 @@
+
+from turtle import Turtle, Screen, mainloop
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_foward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_left():
+
+    tim.left(10)
+
+
+def turn_right():
+    tim.right(10)
+
+
+screen.listen()
+screen.onkey(move_foward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_left, "a")
+screen.onkey(turn_right, "d")
+mainloop()
